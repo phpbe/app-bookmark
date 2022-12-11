6914c44d788e8102aede50a8d39e545f0c80643e
Lou Barnes <8981024@qq.com>
2022-12-11 17:59:19 +0800

    图标

--- src/Property.php
+++ src/Property.php
@@ -7,7 +7,7 @@ class Property extends \Be\App\Property
 {
 
     protected string $label = '书签';
-    protected string $icon = 'bi-cart';
+    protected string $icon = 'bi-bookmark';
     protected string $description = '书签（网址收藏）';
 
     public function __construct() {
